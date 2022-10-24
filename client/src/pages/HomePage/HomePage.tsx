fd475e054a4ec7e39d5fc77c4f48f7ae19e0a343
Vanaldito <fredimanuelb@gmail.com>
2022-10-24 17:48:28 -0600

    feat: add the header of the home page

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -14,6 +14,19 @@ export default function HomePage() {
           />
         </a>
       </nav>
+      <header className="header">
+        <div className="header__hero-image">
+          <div className="header__hero-text">
+            <h1 className="header__hero-text__title">CatWiki</h1>
+            <p className="header__hero-text__description">
+              Get to know more about your cat breed
+            </p>
+            <button className="header__search-button" type="button">
+              Search
+            </button>
+          </div>
+        </div>
+      </header>
     </main>
   );
 }
