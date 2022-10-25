a4b16c3f9b4fc595b6b0ea8844a69d362d974b7c
Vanaldito <fredimanuelb@gmail.com>
2022-10-24 19:55:45 -0600

    feat: add a section explaining why you should have a cat

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -7,6 +7,23 @@ export default function HomePage() {
     <main className="home-page">
       <Navbar />
       <Header />
+      <section className="have-a-cat">
+        <h2 className="have-a-cat__title">Why should you have a cat?</h2>
+        <p className="have-a-cat__reasons">
+          Having a cat around you can actually trigger the release of calming
+          chemicals in your body which lower your stress and anxiety levels
+        </p>
+        <div className="have-a-cat__images">
+          {[2, 3, 1].map(number => (
+            <img
+              src={`/image ${number}.png`}
+              alt="Have a cat"
+              key={number}
+              className="have-a-cat__image"
+            />
+          ))}
+        </div>
+      </section>
     </main>
   );
 }
