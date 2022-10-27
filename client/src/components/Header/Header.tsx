3e0b895e975ae301796adb05d6c8e2b34fe48a29
Vanaldito <fredimanuelb@gmail.com>
2022-10-27 08:36:25 -0600

    feat: add a search bar

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,28 +1,42 @@
+import { useState } from "react";
+import { SearchBar } from "../SearchBar";
+
 import "./Header.css";
 
 export default function Header() {
+  const [displaySearchBar, setDisplaySearchBar] = useState(false);
+
   return (
-    <header className="header">
-      <div className="header__hero-image">
-        <div className="header__hero-text">
-          <h1 className="header__hero-text__title">CatWiki</h1>
-          <p className="header__hero-text__description">
-            Get to know more about your cat breed
-          </p>
-          <button className="header__search-button" type="button">
-            Search
-          </button>
+    <>
+      <header className="header">
+        <div className="header__hero-image">
+          <div className="header__hero-text">
+            <h1 className="header__hero-text__title">CatWiki</h1>
+            <p className="header__hero-text__description">
+              Get to know more about your cat breed
+            </p>
+            <button
+              className="header__search-button"
+              type="button"
+              onClick={() => setDisplaySearchBar(true)}
+            >
+              Search
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="header__most-searched-breeds">
-        <h2 className="header__most-searched-breeds__title">
-          Most Searched Breeds
-        </h2>
-        <p className="header__most-searched-breeds__discover">
-          66+ Breeds For you to discover
-        </p>
-        <div className="header__most-searched-breeds__breeds"></div>
-      </div>
-    </header>
+        <div className="header__most-searched-breeds">
+          <h2 className="header__most-searched-breeds__title">
+            Most Searched Breeds
+          </h2>
+          <p className="header__most-searched-breeds__discover">
+            66+ Breeds For you to discover
+          </p>
+          <div className="header__most-searched-breeds__breeds"></div>
+        </div>
+      </header>
+      {displaySearchBar && (
+        <SearchBar closeBar={() => setDisplaySearchBar(false)} />
+      )}
+    </>
   );
 }
---
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,29 @@
+import "./SearchBar.css";
+
+interface SearchBarProps {
+  closeBar: () => void;
+}
+
+export default function SearchBar({ closeBar }: SearchBarProps) {
+  return (
+    <div className="search-bar">
+      <button onClick={closeBar} className="search-bar__close-button">
+        Close
+      </button>
+      <form
+        className="search-bar__form"
+        onSubmit={event => event.preventDefault()}
+      >
+        <input
+          className="search-bar__field"
+          type="text"
+          placeholder="Enter your breed"
+        />
+        <button className="search-bar__search-button" type="submit">
+          Search
+        </button>
+        <div className="search-bar__recommendations"></div>
+      </form>
+    </div>
+  );
+}
