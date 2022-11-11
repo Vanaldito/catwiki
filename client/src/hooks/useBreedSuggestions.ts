1766081795753a80e9f0fb4398ec103ece277785
Vanaldito <fredimanuelb@gmail.com>
2022-11-10 18:15:29 -0600

    fix: remove suggestions call timeout

--- client/src/hooks/useBreedSuggestions.ts
+++ client/src/hooks/useBreedSuggestions.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BreedsResponse } from "../models";
 import { searchBreeds } from "../services";
 
@@ -6,36 +6,29 @@ export default function useBreedSuggestions(): [
   BreedsResponse | null,
   (query: string) => void
 ] {
-  const [currentTimeout, setCurrentTimeout] = useState<NodeJS.Timeout | null>(
-    null
-  );
-  const [currentController, setCurrentController] =
-    useState<AbortController | null>(null);
+  const [controller, setController] = useState<AbortController | null>(null);
   const [suggestions, setSuggestions] = useState<BreedsResponse | null>(null);
 
   function getSuggestions(query: string) {
-    // Abort the previous request because the user is typing.
-    currentController?.abort();
-    if (currentTimeout) clearTimeout(currentTimeout);
-
     if (!query) return setSuggestions(null);
 
-    setCurrentTimeout(
-      // Call the api after 400ms to make sure the user stopped typing.
-      setTimeout(() => {
-        const { controller, call } = searchBreeds(query);
+    const { controller, call } = searchBreeds(query);
 
-        setCurrentController(controller);
+    setController(controller);
 
-        call.then(data => {
-          if (data.status === 200 && data.info) {
-            setSuggestions(data);
-            setCurrentController(null);
-          }
-        });
-      }, 400)
-    );
+    call.then(data => {
+      if (data.status === 200 && data.info) {
+        setSuggestions(data);
+      }
+      setController(null);
+    });
   }
+
+  useEffect(() => {
+    return () => {
+      controller?.abort();
+    };
+  }, []);
 
   return [suggestions, getSuggestions];
 }
