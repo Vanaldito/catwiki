c52d728a50e15bd85bbcd7616ac243fe73c9892c
Vanaldito <fredimanuelb@gmail.com>
2022-11-17 13:46:26 -0600

    feat: use lazy loading to load the pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,27 @@
+import React, { lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-
-import { BreedPage, HomePage, MostSearchedBreedsPage } from "./pages";
+import { Loader } from "./components";
 
 import "./App.css";
 
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const BreedPage = lazy(() => import("./pages/BreedPage/BreedPage"));
+const MostSearchedBreedsPage = lazy(
+  () => import("./pages/MostSearchedBreedsPage/MostSearchedBreedsPage")
+);
+
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/breed" element={<BreedPage />} />
-      <Route
-        path="/most-searched-breeds"
-        element={<MostSearchedBreedsPage />}
-      />
-      <Route path="/*" element={<div>404 Not Found</div>} />
-    </Routes>
+    <React.Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/breed" element={<BreedPage />} />
+        <Route
+          path="/most-searched-breeds"
+          element={<MostSearchedBreedsPage />}
+        />
+        <Route path="/*" element={<div>404 Not Found</div>} />
+      </Routes>
+    </React.Suspense>
   );
 }
